e2f9a145e084ef329215fb65d2adb71a231b334d
Mariano <mcapielo@gmail.com>
2022-08-10 23:42:17 -0300

    Fix Build 6

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
+
 
 
 import './404.css'
 
 function NotFound() {
+	const [t] = useTranslation('global')
 	return (
 		<div className='404'>
 			<h1>Not Found!</h1>
