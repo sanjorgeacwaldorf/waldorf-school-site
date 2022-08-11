573d4dc3ad8bffd30517e497e65915f75628ff6c
Mariano <mcapielo@gmail.com>
2022-08-11 09:15:12 -0300

    Primera version del lazy loading de youtube - Feo, pero funciona

--- src/components/about2/About2.tsx
+++ src/components/about2/About2.tsx
@@ -12,7 +12,7 @@ const About2 = () => {
 				<div className="row">
 					<div className="embed-responsive embed-responsive-16by9 about2-video mb-4">
 						<iframe title="Puente de Estrellas" frameBorder="0" className='embed-responsive-item' width="100%" height="400"
-							src="https://www.youtube.com/embed/WtVDcvX0o3o" loading="lazy"
+							srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/WtVDcvX0o3o/?autoplay=1><img src=https://img.youtube.com/vi/WtVDcvX0o3o/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? Wisecrack Edition'><span>▶</span></a>"							
 							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 							allowFullScreen
 						>
