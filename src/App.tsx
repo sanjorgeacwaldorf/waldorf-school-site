24702778407755c8a4a3d070efd4083718427eaf
Mariano <mcapielo@gmail.com>
2023-07-04 13:58:19 -0300

    fix building

--- src/App.tsx
+++ src/App.tsx
@@ -52,34 +52,34 @@ function App() {
 		}
 	}
 
-	const datosEstructuradosEducationalOrganization =  {
-		"@context": "https://schema.org",
-		"@type": "EducationalOrganization",
-		"name": t('seo.title'),
-		"description": t('seo.description'),
-		"url": "https://sanjorgeac.com.ar/",
-		"logo": "https://sanjorgeac.com.ar/logo.png",
-		"address": {
-			'@type': 'PostalAddress',
-			'streetAddress': 'Isaac Newton 5827',
-			'addressLocality': 'Villa Belgrano',
-			'addressRegion': 'Córdoba Capital',
-			'postalCode': '5006',
-			'addressCountry': 'Argentina'
-		},
-		"telephone": " '+54-9-3543-428329'",
-		"email": "donaciones.sanjorge.waldorf@gmail.com",
-		"openingHours": "Mo-Fr 08:00-13:00",
-		"image": [
-		  "https://www.ejemploescuelaprimaria.com/imagen1.jpg",
-		  "https://www.ejemploescuelaprimaria.com/imagen2.jpg"
-		],
-		"sameAs": [
-		  "https://www.facebook.com/ejemploescuelaprimaria",
-		  "https://twitter.com/ejemploescuela",
-		  "https://www.instagram.com/ejemploescuelaprimaria"
-		]
-	  }
+	// const datosEstructuradosEducationalOrganization =  {
+	// 	'@context': 'https://schema.org',
+	// 	'@type': 'EducationalOrganization',
+	// 	'name': t('seo.title'),
+	// 	'description': t('seo.description'),
+	// 	'url': 'https://sanjorgeac.com.ar/',
+	// 	'logo': 'https://sanjorgeac.com.ar/logo.png',
+	// 	'address': {
+	// 		'@type': 'PostalAddress',
+	// 		'streetAddress': 'Isaac Newton 5827',
+	// 		'addressLocality': 'Villa Belgrano',
+	// 		'addressRegion': 'Córdoba Capital',
+	// 		'postalCode': '5006',
+	// 		'addressCountry': 'Argentina'
+	// 	},
+	// 	'telephone': '+54-9-3543-428329',
+	// 	'email': 'donaciones.sanjorge.waldorf@gmail.com',
+	// 	'openingHours': 'Mo-Fr 08:00-13:00',
+	// 	'image': [
+	// 		'https://www.ejemploescuelaprimaria.com/imagen1.jpg',
+	// 		'https://www.ejemploescuelaprimaria.com/imagen2.jpg'
+	// 	],
+	// 	'sameAs': [
+	// 	  'https://www.facebook.com/ejemploescuelaprimaria',
+	// 	  'https://twitter.com/ejemploescuela',
+	// 	  'https://www.instagram.com/ejemploescuelaprimaria'
+	// 	]
+	// }
 
 	const datosEstructuradosLocalBusiness = {
 		'@context': 'https://schema.org',
