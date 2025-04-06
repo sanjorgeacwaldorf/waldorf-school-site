a4575dc22d1982b10a0d4469fbdb92d067316ce0
JulitoGarcia <juliogarcianassiz@gmail.com>
2025-04-05 22:06:30 -0300

    se cambia logo por el de secundario

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logoPuente from '../../media/LogoEscuelaJardinPrimaria.webp'
+import logoPuente from '../../media/logosecu.png'
 
 import { useTranslation } from 'react-i18next'
 
