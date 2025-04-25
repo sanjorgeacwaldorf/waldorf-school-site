551ad61d0e25f4d44e10c3329045060d73e6106a
JulitoGarcia <juliogarcianassiz@gmail.com>
2025-04-25 13:41:06 -0300

    Se agrega video en landing

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Landing.css'
-//import landingVideoMP4 from '../../media/video_landing_30s_2.mp4'
+import landingVideoMP4 from '../../media/Casa_Secundaria_1080.mp4'
 //import landingVideoWEBM from '../../media/video_landing_30s_2.webm'
-//import landingVideoPoster from '../../media/poster.jpg'
+import landingVideoPoster from '../../media/poster.jpg'
 import { useTranslation } from 'react-i18next'
 
 
@@ -14,10 +14,9 @@ const Landing = () => {
 	return (
 		<div className="landing-container-box">
 			<div className='landing-container'>
-				{/* <video className='video' poster={landingVideoPoster} preload='none' autoPlay loop muted >
-					<source src={landingVideoWEBM} type='video/webm' />
+				<video className='video' poster={landingVideoPoster} preload='none' autoPlay loop muted >
 					<source src={landingVideoMP4} type='video/mp4' />
-				</video> */}
+				</video>
 				<h1> {t('landing.title')}</h1>
 				<p>
 					{t('landing.text')}
